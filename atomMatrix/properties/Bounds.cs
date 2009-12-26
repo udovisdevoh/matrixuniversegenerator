7977d5ac6549d3ace24f458dace5fb0d3dd24590
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 20:48:52 +0000

    Added some more comments Added atom matrix set

--- atomMatrix/properties/Bounds.cs
+++ atomMatrix/properties/Bounds.cs
@@ -5,48 +5,81 @@ using System.Text;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Represents a selection bounds
+    /// </summary>
     public class Bounds
     {
         #region Fields
+        /// <summary>
+        /// Left bound
+        /// </summary>
         private int left = 0;
 
+        /// <summary>
+        /// Right bound
+        /// </summary>
         private int right = 0;
 
+        /// <summary>
+        /// Top bound
+        /// </summary>
         private int top = 0;
 
+        /// <summary>
+        /// Bottom bound
+        /// </summary>
         private int bottom = 0;
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Left bound
+        /// </summary>
         public int Left
         {
             get { return left; }
             set { left = value; }
         }
 
+        /// <summary>
+        /// Right bound
+        /// </summary>
         public int Right
         {
             get { return right; }
             set { right = value; }
         }
 
+        /// <summary>
+        /// Top bound
+        /// </summary>
         public int Top
         {
             get { return top; }
             set { top = value; }
         }
 
+        /// <summary>
+        /// Bottom bound
+        /// </summary>
         public int Bottom
         {
             get { return bottom; }
             set { bottom = value; }
         }
 
+        /// <summary>
+        /// Width
+        /// </summary>
         public int Width
         {
             get { return right - left + 1; }
         }
 
+        /// <summary>
+        /// Height
+        /// </summary>
         public int Height
         {
             get { return bottom - top + 1; }
