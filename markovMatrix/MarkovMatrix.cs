23948eaba4155be226925f66162ce40b63f539bd
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 21:05:02 +0000

    Added some more comments

--- markovMatrix/MarkovMatrix.cs
+++ markovMatrix/MarkovMatrix.cs
@@ -5,15 +5,28 @@ using System.Text;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Markov matrix
+    /// </summary>
     class MarkovMatrix
     {
         #region Fields
+        /// <summary>
+        /// Internal matrix
+        /// </summary>
         private float[,] internalMatrix;
 
+        /// <summary>
+        /// Matrix size (width or height)
+        /// </summary>
         private int matrixSize;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Create markov matrix
+        /// </summary>
+        /// <param name="matrixSize">desired size (width or height)</param>
         public MarkovMatrix(int matrixSize)
         {
             this.matrixSize = matrixSize;
@@ -22,6 +35,11 @@ namespace anticulturematrix
         #endregion
 
         #region Public Methods
+        /// <summary>
+        /// Get row for atom type
+        /// </summary>
+        /// <param name="atom">atom</param>
+        /// <returns>row for atom type</returns>
         public float[] GetRow(Atom atom)
         {
             float[] row = new float[matrixSize];
@@ -30,17 +48,22 @@ namespace anticulturematrix
             return row;
         }
 
-        public int GetRowMax(int i)
+        /// <summary>
+        /// Maximum key in specified row
+        /// </summary>
+        /// <param name="rowId">rowId</param>
+        /// <returns>Maximum key in specified row</returns>
+        public int GetRowMax(int rowId)
         {
             int maxKey = -1;
             float maxValue = -1.0f;
 
             for (int j = 0; j < matrixSize; j++)
             {
-                if (maxKey == -1 || internalMatrix[i, j] > maxValue)
+                if (maxKey == -1 || internalMatrix[rowId, j] > maxValue)
                 {
                     maxKey = j;
-                    maxValue = internalMatrix[i, j];
+                    maxValue = internalMatrix[rowId, j];
                 }
             }
             return maxKey;
@@ -48,12 +71,21 @@ namespace anticulturematrix
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Value at specified coordinates
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>Value at specified coordinates</returns>
         public float this[int x, int y]
         {
             get { return internalMatrix[x, y]; }
             set { internalMatrix[x, y] = value; }
         }
 
+        /// <summary>
+        /// Matrix size (height or width)
+        /// </summary>
         public int MatrixSize
         {
             get { return matrixSize; }
