79cf6c8e285216fc52dc428b39689f1d08ed3955
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 20:59:13 +0000

    Added some more comments

--- trunk/atomMatrix/AtomMatrixSet.cs
+++ trunk/atomMatrix/AtomMatrixSet.cs
@@ -5,7 +5,68 @@ using System.Text;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Atom matrix set
+    /// </summary>
     class AtomMatrixSet
     {
+        #region Fields
+        /// <summary>
+        /// Width
+        /// </summary>
+        private int width;
+
+        /// <summary>
+        /// Height
+        /// </summary>
+        private int height;
+
+        /// <summary>
+        /// Internal matrix
+        /// </summary>
+        private AtomMatrix[,] internalMatrix;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Create atom matrix set
+        /// </summary>
+        /// <param name="width">desired width</param>
+        /// <param name="height">desired height</param>
+        public AtomMatrixSet(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+            internalMatrix = new AtomMatrix[width, height];
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// AtomMatrix in the set
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>selected atom matrix</returns>
+        public AtomMatrix this[int x, int y]
+        {
+            get
+            {
+                if (x >= width || y >= height || x < 0 || y < 0)
+                    return null;
+
+                return internalMatrix[x, y];
+            }
+            set { internalMatrix[x, y] = value; }
+        }
+
+        /// <summary>
+        /// How many atom matrix in the set (width * height)
+        /// </summary>
+        public int Count
+        {
+            get { return width * height; }
+        }
+        #endregion
     }
 }
