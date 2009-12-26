040d6d90db4e292718eb038b84a2c72f041000bd
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 20:51:35 +0000

    Added some more comments

--- trunk/atomMatrix/AtomMatrix.cs
+++ trunk/atomMatrix/AtomMatrix.cs
@@ -5,17 +5,34 @@ using System.Text;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Atom matrix
+    /// </summary>
     public class AtomMatrix
     {
         #region Fields
+        /// <summary>
+        /// Width
+        /// </summary>
         private int width;
 
+        /// <summary>
+        /// Height
+        /// </summary>
         private int height;
 
+        /// <summary>
+        /// Internal matrix
+        /// </summary>
         private Atom[,] internalMatrix;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Create atom matrix
+        /// </summary>
+        /// <param name="width">desired width</param>
+        /// <param name="height">desired height</param>
         public AtomMatrix(int width, int height)
         {
             this.width = width;
@@ -26,16 +43,28 @@ namespace anticulturematrix
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Width
+        /// </summary>
         public int Width
         {
             get { return width; }
         }
 
+        /// <summary>
+        /// Height
+        /// </summary>
         public int Height
         {
             get { return height; }
         }
 
+        /// <summary>
+        /// Atom in the matrix
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>atom</returns>
         public Atom this[int x, int y]
         {
             get
@@ -48,6 +77,9 @@ namespace anticulturematrix
             set{ internalMatrix[x, y] = value; }
         }
 
+        /// <summary>
+        /// How many atom in the matrix (width * height)
+        /// </summary>
         public int Count
         {
             get { return width * height; }
