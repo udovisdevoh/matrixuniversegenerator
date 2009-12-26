5a42e0d834814ffaf58f2cbdc725843556583d77
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 20:48:52 +0000

    Added some more comments Added atom matrix set

--- trunk/atomMatrix/Atom.cs
+++ trunk/atomMatrix/Atom.cs
@@ -6,19 +6,35 @@ using System.Drawing;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Atom (colored pixel) in the matrix
+    /// </summary>
     public class Atom
     {
         #region Fields
+        /// <summary>
+        /// Color
+        /// </summary>
         private Color color;
 
+        /// <summary>
+        /// Type Id
+        /// </summary>
         private int id;
         #endregion
 
         #region Static
+        /// <summary>
+        /// Id counter
+        /// </summary>
         private static int idCounter = 0;
         #endregion
 
         #region Constructor
+        /// <summary>
+        /// Create an atom
+        /// </summary>
+        /// <param name="color">desired color</param>
         public Atom(Color color)
         {
             this.color = color;
@@ -28,11 +44,17 @@ namespace anticulturematrix
         #endregion
 
         #region Properties
+        /// <summary>
+        /// Color
+        /// </summary>
         public Color Color
         {
             get { return color; }
         }
 
+        /// <summary>
+        /// Type id
+        /// </summary>
         public int Id
         {
             get { return id; }
