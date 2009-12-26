ebae6c876fe48c30363f87b7e4892da327f88760
glacasse <glacasse@d1e4b272-f00d-11de-af0a-9d584d74d99e>
2009-12-26 21:06:42 +0000

    Added some more comments

--- trunk/probabilities/Probabilities.cs
+++ trunk/probabilities/Probabilities.cs
@@ -5,20 +5,24 @@ using System.Text;
 
 namespace anticulturematrix
 {
+    /// <summary>
+    /// Used for probabilities and random number generators
+    /// </summary>
     static class Probabilities
     {
         #region Fields
+        /// <summary>
+        /// Random number generator
+        /// </summary>
         private static Random random = new Random();
         #endregion
 
-        #region Properties
-        public static Random Random
-        {
-            get { return random; }
-        }
-        #endregion
-
         #region Public Methods
+        /// <summary>
+        /// Get ponderated ranom key from row
+        /// </summary>
+        /// <param name="row">matrix row</param>
+        /// <returns>ponderated ranom key from row</returns>
         public static int GetPonderatedRandomKey(float[] row)
         {
             float sum = Sum(row);
@@ -40,12 +44,27 @@ namespace anticulturematrix
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Sum of matrix row values
+        /// </summary>
+        /// <param name="row">matrix row</param>
+        /// <returns>Sum of matrix row values</returns>
         private static float Sum(float[] row)
         {
             float sum = 0.0f;
             foreach (float number in row)
                 sum += number;
             return sum;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Random number generator
+        /// </summary>
+        public static Random Random
+        {
+            get { return random; }
         }
         #endregion
     }
